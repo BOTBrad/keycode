1e64c1df206dbef4cf1721dae6db027eca8663d0
BOTBrad <real.bot.brad@gmail.com>
2016-08-31 00:01:14 -0400

    update for elm 0.17

--- src/Keycode.elm
+++ src/Keycode.elm
@@ -1,11 +1,33 @@
-module Keycode where
+module Keycode exposing (..)
 
-import Graphics.Element exposing (show)
+import Char
+import Html
+import Html.App as App
 import Keyboard
+import Platform.Cmd as Cmd
+import String
 
 {-| main: where all the fun happens
 
 right now it just shows the time since the program started updated at 10 fps
 -}
 main =
-  Keyboard.presses |> Signal.map show
+  App.program
+  { init = (' ', Cmd.none)
+  , subscriptions = (\_ -> Keyboard.downs (Key << Char.fromCode))
+  , view = String.fromChar >> Html.text
+  , update = update
+  }
+
+type Msg
+  = Key Char
+
+update : Msg -> Char -> (Char, Cmd Msg)
+update m mod =
+  let
+    key = case m of
+      Key c ->
+        c
+  in
+    (key, Cmd.none)
+
